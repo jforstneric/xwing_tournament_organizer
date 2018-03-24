18ae820d185efc57b005290b333a760fdddad0ec
Jure Forstneric <jforst@gmail.com>
2018-03-24 20:48:24 +0100

    Basic structure done, adding players, sorting, calculating points.

--- addParticipants.py
+++ addParticipants.py
@@ -5,10 +5,12 @@
 #jforst at google's email service
 #2018-03-18
 
-import random
+from random import shuffle
+from operator import itemgetter
+
 
 def addParticipants():
-	allParticipants = {}
+	allParticipants = []
 	participantNumber = 1
 	while True:
 		print('Enter participant #' + str(participantNumber) + ': (or press enter to continue...)')
@@ -16,27 +18,76 @@ def addParticipants():
 		if participant == "":
 			print('Our participants are:')
 			for p in allParticipants:
-				print(str(p))
-			if len(allParticipants) % 2 != 0:
-				allParticipants['Bye'] = [0,0]
+				print(str(p[0]))
+			if len(allParticipants) % 2 != 0:		#if list of participants == odd, add a bye
+				allParticipants.append(['Bye',0,0])
 			break
-		allParticipants[participant] = [0,0]
+		allParticipants.append([participant,0,0])
 		print('Our participants are:')
 		for p in allParticipants:
-			print(str(p))
+			print(str(p[0]))
 		participantNumber += 1
-	return(allParticipants)		#at the moment, this is a dictionary with the participant names as the keys and 0,0 as the values
+	return(allParticipants)		#a list with participant's names
 	
 def firstSort(allParticipants):
-	sortingList = list(allParticipants)		#this changes the dictionary into a list with [name, 0, 0] as each value in the list
+	shuffle(allParticipants)
+	print('List after random shuffle:')
+	print(allParticipants)
+	return allParticipants
+	
+def printMatches(sortedList):
+	for p in range(0,len(sortedList),2):
+		print(sortedList[p][0] + ' plays ' + sortedList[(p+1)][0])
+		
+def scoreMatches(sortedList):
+	for p in range(0,len(sortedList),2):
+		print('Enter ' + sortedList[p][0] + '\'s score in ship points (max. 100):')
+		playScore1 = int(input())
+		print('Enter ' + sortedList[p+1][0] + '\'s score in ship points (max. 100):')
+		playScore2 = int(input())
+		if playScore1 == playScore2:			#in case of tie
+			sortedList[p][1] = (sortedList[p][1] + 1)
+			sortedList[p][2] = (sortedList[p][2] + 100)
+			sortedList[p+1][1] = (sortedList[p+1][1] + 1)
+			sortedList[p+1][2] = (sortedList[p+1][2] + 100)
+		if 1 <= abs(playScore1 - playScore2) < 12:		#in case of modified win (difference in MOV < 12
+			if playScore1 > playScore2:
+				sortedList[p][1] = (sortedList[p][1] + 3)
+				sortedList[p][2] = (sortedList[p][2] + (100 + (playScore1 - playScore2)))
+				sortedList[p+1][2] = (sortedList[p+1][2] + (100 - (playScore1 - playScore2)))
+			else:
+				sortedList[p+1][1] = (sortedList[p+1][1] + 3)
+				sortedList[p+1][2] = (sortedList[p+1][2] + (100 + (playScore2 - playScore1)))
+				sortedList[p][2] = (sortedList[p][2] + (100 - (playScore2 - playScore1)))
+		else:			#normal win
+			if playScore1 > playScore2:
+				sortedList[p][1] = (sortedList[p][1] + 5)
+				sortedList[p][2] = (sortedList[p][2] + (100 + (playScore1 - playScore2)))
+				sortedList[p+1][2] = (sortedList[p+1][2] + (100 - (playScore1 - playScore2)))
+			else:
+				sortedList[p+1][1] = (sortedList[p+1][1] + 5)
+				sortedList[p+1][2] = (sortedList[p+1][2] + (100 + (playScore2 - playScore1)))
+				sortedList[p][2] = (sortedList[p][2] + (100 - (playScore2 - playScore1)))
+	print(sortedList)
+	return(sortedList)
+	
+def otherSorting(sortedList):
+	sortedList = sorted(sortedList, key=itemgetter(1,2), reverse=True)		#double sort with itemgetter - first by sortedList[1] then by sortedList[2]
+	return(sortedList)
 	
 
 if __name__ == '__main__':
 	allParticipants = addParticipants()
-	print('Done with adding participants, they are:')
-#	print(allParticipants)
-	for p in allParticipants:
-		print(str(p))
-	firstSort(allParticipants)
+	print('How many rounds are we playing today?')
+	playRounds = int(input())
+	sortedList = firstSort(allParticipants)
+	printMatches(sortedList)
+	#this should be looped for the number of rounds:
+	for r in range(playRounds):
+		postScoring = scoreMatches(sortedList)
+		sortedList = otherSorting(postScoring)		#here we should show the list to the user and ask if they want to manually assign pairs!
+		printMatches(sortedList)
+	
+	
 
 
